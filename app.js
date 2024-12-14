9322cc77ec3c754c14936d1a1c0785a6d18b9e5f
Pintu Kumar Sharma <pintusharma6803@gmail.com>
2024-12-14 11:00:43 +0530

    using middleware

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!')
+app.use(morgan('dev'))  // 3rd party middleware
+
+app.set("view engine",'ejs')
+
+app.use((req,res,next)=>{             // middleware
+    console.log("this is middleware")
+    const a=5
+    const b=10
+    console.log(a+b)
+    return next()
 })
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
 app.get('/about',(req,res)=>{
     res.send('This is the about page')
 })
+
 app.get('/profile',(req,res)=>{
     res.send("This is the profile page")
 })
