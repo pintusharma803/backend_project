504dbf43056130336caa9ad5622bf98b57c17768
Pintu Kumar Sharma <pintusharma6803@gmail.com>
2024-12-14 10:27:44 +0530

    express create

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const http = require('http')
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    if(req.url == "/about"){
-        res.end("The about page")
-    }
-    if(req.url == "/profile"){
-        res.end("The profile page")
-    }
-    if(req.url == '/'){
-        res.end('The Home Page')
-    }
-    
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
+app.get('/about',(req,res)=>{
+    res.send('This is the about page')
+})
+app.get('/profile',(req,res)=>{
+    res.send("This is the profile page")
 })
 
-
-server.listen(3000)
+app.listen(3000)
