b26f9450c680318a2593202f45d7e8125a8cfbc7
Pintu Kumar Sharma <pintusharma6803@gmail.com>
2024-12-14 11:52:31 +0530

    adding css

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const app = express();
 
 app.use(morgan('dev'))  // 3rd party middleware
 
-app.set("view engine",'ejs')
+app.use(express.json())       // ye body method me aaye data ko json me convert karta hai (middleware hai)
+app.use(express.urlencoded({extended:true}))       //ye bhi use karna padta hai json data ko parse karna hai to
+app.use(express.static("public"))     // use for css file and images and other static files
 
-app.get('/',(req,res,next)=>{
-    const a = 5
-    const b = 10
-    console.log(a+b)
-    next()
-},(req,res)=>{
+app.set("view engine",'ejs')
+app.get('/',(req,res)=>{
     res.render('index')
 })
 
@@ -21,6 +19,11 @@ app.get('/about',(req,res)=>{
 
 app.get('/profile',(req,res)=>{
     res.send("This is the profile page")
+})
+
+app.post('/get-form-data',(req,res)=>{
+    console.log(req.body)      
+    res.send('data received')
 })
 
 app.listen(3000)
